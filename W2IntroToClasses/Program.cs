20da1018675f10f70544325c488875040911075d
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-17 16:48:01 -0500

    Add project files.

---
+++ W2IntroToClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace W2IntroToClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Product p1 = new Product();
+            Product p2 = new Product(101, "Laptop", 1200);
+        }
+    }
+}
