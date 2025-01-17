71e3997811fa6b62add7828c277706f6681da7c1
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-17 17:51:25 -0500

    complete

--- W2IntroToClasses/Program.cs
+++ W2IntroToClasses/Program.cs
@@ -8,22 +8,41 @@
             //Product p2 = new Product(101, "Laptop", 1200);
 
 
-            AccountWithMethods myAcc = new AccountWithMethods(500);
-            Console.WriteLine("Balance = " + myAcc.GetBalance().ToString("C"));
+            //AccountWithMethods myAcc = new AccountWithMethods(500);
+            //Console.WriteLine("Balance = " + myAcc.GetBalance().ToString("C"));
+
+            //myAcc.Deposit(100);
+            //Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
+
+            //try
+            //{
+            //    myAcc.Withdraw(3000);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
+
+
+
+            AccountWithProperties myAcc = new AccountWithProperties(500);
+            Console.WriteLine("Balance = " + myAcc.Balance.ToString("C"));
 
             myAcc.Deposit(100);
-            Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
+            Console.WriteLine("\nBalance = " + myAcc.Balance.ToString("C"));
 
             try
             {
-                myAcc.Withdraw(3000);
+                myAcc.Withdraw(300);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
-            Console.WriteLine("\nBalance = " + myAcc.GetBalance().ToString("C"));
+            Console.WriteLine("\nBalance = " + myAcc.Balance.ToString("C"));
         }
     }
 }
